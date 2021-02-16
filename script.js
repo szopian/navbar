384a09a686d8327bce67029640884baa84540c90
Adri Ana <szopiangreg@gmail.com>
2021-02-16 01:17:14 +0000

    work in progress

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function toggleClass() {
-  let menu = document.querySelector(".nav__links");
-  menu.classList.toggle("toggleMenu");
+const mainMenu = document.querySelector(".main__menu");
+const closeMenu = document.querySelector(".close__menu");
+const openMenu = document.querySelector(".open__menu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+
+function close() {
+  mainMenu.style.top = "-100%";
 }
